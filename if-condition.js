fe6f59425eef22b1c23d1820e1b75b0a249704b7
Sarah Kuss <sarahkuss@Sarahs-MacBook-Air.local>
2023-03-21 12:00:09 -0400

    just learned ternary

---
+++ if-condition.js
@@ -0,0 +1,9 @@
+const currentHour = 20
+if (currentHour <= 12) {
+    console.log('good morning')
+} else if (currentHour <= 18) {
+    console.log('good afternoon')
+} else {
+    console.log('good night')
+}
+
